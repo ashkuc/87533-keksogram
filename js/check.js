887bd60e591b93497d2d98348b7e42dc099b44f5
ashkuc <arsenymedoev@mail.ru>
2015-11-13 01:58:39 +0300

    Update check.js
    added sum and square functions

--- js/check.js
+++ js/check.js
@@ -1,4 +1,4 @@
-var getMessage = new function(a, b){
+function getMessage(a, b){
   switch (typeof(a)){
     case 'boolean':
       if(a){
@@ -11,16 +11,30 @@ var getMessage = new function(a, b){
     case 'number':
       return('Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' аттрибутов');
       break;
+      //not every object is array, but works for now
     case 'object':
       if(typeof(b)=='object'){
-        return('Общая площадь артефактов сжатия: [square] пикселей');
+        return('Общая площадь артефактов сжатия: ' + square(a, b) + ' пикселей');
       }
       else{
-        return('Количество красных точек во всех строчках изображения: [sum]');
+        return('Количество красных точек во всех строчках изображения: ' + sum(a));
       }
       break;
   }
 };
 // going to add square and sum functions soon
-var square = new function(aa, bb){};
-var square = new function(aa){};
+function square(aa, bb){
+  var len= (aa.length >= bb.length) ? aa.length-1 : bb.length-1;
+  var result = 0;
+  for(len; len >= 0; len--){
+    result += aa[len] * bb[len];
+  };
+  return result;
+};
+function sum(aa){
+  var result = 0;
+  for(len = 0; len < aa.length; len++){
+    result += aa[len];
+  };
+  return result;  
+};
