f241b0f0f9d714a27c5401cd4b67a62ec7eb281a
ashkuc <arsenymedoev@mail.ru>
2015-11-14 18:06:32 +0300

    Update check.js
    В функции sum объявил переменную локально. Назвал её i - стандартно для счётчика в простой функции.
    Добавил отступы.

--- js/check.js
+++ js/check.js
@@ -1,40 +1,41 @@
-function getMessage(a, b){
-  switch (typeof(a)){
-    case 'boolean':
-      if(a){
-        return('Переданное GIF-изображение анимировано и содержит '+b+' кадров');
-      }
-      else{
-        return('Переданное GIF-изображение не анимировано');
-      }
-      break;
-    case 'number':
-      return('Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' аттрибутов');
-      break;
-      //not every object is array, but works for now
-    case 'object':
-      if(typeof(b)=='object'){
-        return('Общая площадь артефактов сжатия: ' + square(a, b) + ' пикселей');
-      }
-      else{
-        return('Количество красных точек во всех строчках изображения: ' + sum(a));
-      }
-      break;
-  }
-};
-// going to add square and sum functions soon
-function square(aa, bb){
-  var len= (aa.length >= bb.length) ? aa.length-1 : bb.length-1;
-  var result = 0;
-  for(len; len >= 0; len--){
-    result += aa[len] * bb[len];
+  function getMessage(a, b){
+    switch (typeof(a)){
+      case 'boolean':
+        if(a){
+          return('Переданное GIF-изображение анимировано и содержит '+b+' кадров');
+        }
+        else{
+          return('Переданное GIF-изображение не анимировано');
+        }
+        break;
+      case 'number':
+        return('Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' аттрибутов');
+        break;
+        //not every object is array, but works for now
+      case 'object':
+        if(typeof(b)=='object'){
+          return('Общая площадь артефактов сжатия: ' + square(a, b) + ' пикселей');
+        }
+        else{
+          return('Количество красных точек во всех строчках изображения: ' + sum(a));
+        }
+        break;
+    }
   };
-  return result;
-};
-function sum(aa){
-  var result = 0;
-  for(len = 0; len < aa.length; len++){
-    result += aa[len];
+
+  function square(array1, array2){
+    var minLength= (array1.length <= array2.length) ? array1.length-1 : array2.length-1;
+    var result = 0;
+    for(minLength; minLength >= 0; minLength--){
+      result += array1[minLength] * array2[minLength];
+    };
+    return result;
   };
-  return result;  
-};
+  
+  function sum(array1){
+    var result = 0;
+    for(var i = 0; i < array1.length; i++){
+      result += array1[i];
+    };
+    return result;  
+  };
