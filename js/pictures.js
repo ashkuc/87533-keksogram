29d5b13073226fcc43d0bc89b5369e2088cb1779
Arseny Medoev <arsenymedoev@mail.ru>
2015-12-13 04:04:57 +0300

    Модуль 4 задание 1

---
+++ js/pictures.js
@@ -0,0 +1,32 @@
+var divFilters = document.querySelector('.filters');
+
+var divPictures = document.querySelector('.pictures');
+
+var templateNode = document.querySelector('#picture-template');
+
+//Создание нового экземпляра по шаблону
+newPictureTemplate = function(item){
+    var newTemplate = templateNode.content.children[0].cloneNode(true);
+    var image = new Image();
+
+    image.onload = function() {
+        image.height = image.width = 182;
+        newTemplate.replaceChild(image, newTemplate.querySelector('img'));
+    };
+
+    image.onerror = function() {
+      newTemplate.classList.add('picture-load-failure');
+    };
+
+    image.src = item.url;
+
+    newTemplate.querySelector('.picture-comments').textContent = item.comments;
+    newTemplate.querySelector('.picture-likes').textContent = item.likes;
+
+    return newTemplate;
+}
+
+pictures.forEach( function(item){
+    var pictureTemp = newPictureTemplate(item);
+    divPictures.appendChild(pictureTemp);
+});
