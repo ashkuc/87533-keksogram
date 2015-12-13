8321295a8ea3fd9b0e5a3f47739c6246ccce69d6
Arseny Medoev <arsenymedoev@mail.ru>
2015-12-13 14:50:22 +0300

    Update pictures.js
    Изменения для трависа

--- js/pictures.js
+++ js/pictures.js
@@ -1,32 +1,34 @@
-var divFilters = document.querySelector('.filters');
+'use strict';
 
-var divPictures = document.querySelector('.pictures');
+(function() {
+    var divPictures = document.querySelector('.pictures');
 
-var templateNode = document.querySelector('#picture-template');
+    var templateNode = document.querySelector('#picture-template');
 
-//Создание нового экземпляра по шаблону
-newPictureTemplate = function(item){
-    var newTemplate = templateNode.content.children[0].cloneNode(true);
-    var image = new Image();
+    //Создание нового экземпляра по шаблону
+    var newPictureTemplate = function(item) {
+        var newTemplate = templateNode.content.children[0].cloneNode(true);
+        var image = new Image();
 
-    image.onload = function() {
-        image.height = image.width = 182;
-        newTemplate.replaceChild(image, newTemplate.querySelector('img'));
-    };
+        image.onload = function() {
+            image.height = image.width = 182;
+            newTemplate.replaceChild(image, newTemplate.querySelector('img'));
+        };
 
-    image.onerror = function() {
-      newTemplate.classList.add('picture-load-failure');
-    };
+        image.onerror = function() {
+          newTemplate.classList.add('picture-load-failure');
+        };
 
-    image.src = item.url;
+        image.src = item.url;
 
-    newTemplate.querySelector('.picture-comments').textContent = item.comments;
-    newTemplate.querySelector('.picture-likes').textContent = item.likes;
+        newTemplate.querySelector('.picture-comments').textContent = item.comments;
+        newTemplate.querySelector('.picture-likes').textContent = item.likes;
 
-    return newTemplate;
-}
+        return newTemplate;
+    }
 
-pictures.forEach( function(item){
-    var pictureTemp = newPictureTemplate(item);
-    divPictures.appendChild(pictureTemp);
-});
+    pictures.forEach( function(item){
+        var pictureTemp = newPictureTemplate(item);
+        divPictures.appendChild(pictureTemp);
+    });
+})();
