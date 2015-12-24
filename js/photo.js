d87d328a8e130d4b04faa1ee27946394a53d635a
ashkuc <arsenymedoev@mail.ru>
2015-12-24 12:30:15 +0300

    travis - photo.js

--- js/photo.js
+++ js/photo.js
@@ -1,26 +1,26 @@
 function Photo(data) {
   this._data = data;
 
- };
+}
 
 Photo.prototype.render = function() {
 	var templateNode = document.querySelector('#picture-template');
-    var newTemplate = templateNode.content.children[0].cloneNode(true);
-    var image = new Image();
+  var newTemplate = templateNode.content.children[0].cloneNode(true);
+  var image = new Image();
 
-    image.onload = function() {
-      image.height = image.width = 182;
-      newTemplate.replaceChild(image, newTemplate.querySelector('img'));
-    };
+  image.onload = function() {
+    image.height = image.width = 182;
+    newTemplate.replaceChild(image, newTemplate.querySelector('img'));
+  };
 
-    image.onerror = function() {
-      newTemplate.classList.add('picture-load-failure');
-    };
+  image.onerror = function() {
+    newTemplate.classList.add('picture-load-failure');
+  };
 
-    image.src = this._data.url;
+  image.src = this._data.url;
 
-    newTemplate.querySelector('.picture-comments').textContent = this._data.comments;
-    newTemplate.querySelector('.picture-likes').textContent = this._data.likes;
+  newTemplate.querySelector('.picture-comments').textContent = this._data.comments;
+  newTemplate.querySelector('.picture-likes').textContent = this._data.likes;
 
-    return newTemplate;
- }
+  return newTemplate;
+};
